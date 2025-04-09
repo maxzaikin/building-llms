5efed0c9a89a72271f0f41687a386bc7910972ae
Maks Zaikin <Max.V.Zaikin@gmail.com>
2025-04-09 23:26:04 +1100

    doc:2

--- general/src/test.py
+++ general/src/test.py
@@ -1,3 +1,17 @@
 from dotenv import load_dotenv
+import os
+import openai
 
 load_dotenv()
+
+en_txt= "Hello, how are you?"
+
+response= openai.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role":"system","content": "You are helpful assistant."},
+        {"role": "user", "content": f'Translate the following English text to French: "{en_txt}"'}
+    ],
+)
+
+print(response.choices[0].message.content)
